4a0695dbae5ffc90c739045ff970117241b4452b
Expired <developingbread@gmail.com>
2023-02-09 21:37:54 -0800

    Modified Group.lua

--- src/group.lua
+++ src/group.lua
@@ -1,11 +1,14 @@
 local Package = script.Parent
 local Tween = require(Package.tween)
+local Await = require(Package.await)
 
 local TweenGroup = {}
 TweenGroup.__index = TweenGroup
 
 type Settings = {
   staggerTweens: number?,
+  delayTweens: number?,
+
   transition: TweenInfo?
 }
 
@@ -24,10 +27,12 @@ function TweenGroup.Play(
   name: string,
   transition: TweenInfo?
 )
-  for _, tween: Tween.Wrapper in self._tweens do
-    tween:Animate(name, self._settings.transition or transition or nil)
-    task.wait(self._settings.staggerTweens or 0)
-  end
+  Await(self._settings.delayTweens or 0, function()
+    for _, tween: Tween.Wrapper in self._tweens do
+      tween:Animate(name, self._settings.transition or transition or nil)
+      Await(self._settings.staggerTweens or 0)
+    end
+  end)
 end
 
 function TweenGroup.Reset(
