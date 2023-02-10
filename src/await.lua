7dacec8a46a776c30ab593a7dae79f168cc41629
Expired <developingbread@gmail.com>
2023-02-09 21:39:38 -0800

    Added comments to Await.lua

--- src/await.lua
+++ src/await.lua
@@ -1,5 +1,11 @@
+-- This is to skip over the task.wait if the time is less than or equal to 0.
+-- Tries to prevent a very small amount of delay when time is 0
+
+-- Usage:
+-- Await(time: number, finished: (...any) -> any?)
+-- Await(1, function() print('Finished') end)
+
 return function (time: number, finished: (...any) -> any?)
-  if time <= 0 then finished(); return end
-  task.wait(time)
+  if time > 0 then task.wait(time) end
   finished()
 end
